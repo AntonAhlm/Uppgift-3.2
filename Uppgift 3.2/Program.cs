c75c8b5c8859bc3eca838c3d2bd4399b41d07886
Anton <>
2023-02-03 09:06:12 +0100

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilka vann fotbolls-VM för damer 2015? : ");
+            string svar = Console.ReadLine();
+            string svar2 = svar.ToLower();
+
+            if ( svar2 == "usa" )
+            {
+                Console.WriteLine("Du har rätt :D");
+            }
+
+        }
+    }
+}
